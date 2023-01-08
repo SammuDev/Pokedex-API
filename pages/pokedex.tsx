99f348ec8334625ae2470b3a8a503903f40b0a17
SammuDev <samucas878@gmail.com>
2023-01-08 20:18:49 -0300

    feat: criando Navbar, adicionando logo e data fetching

--- pages/pokedex.tsx
+++ pages/pokedex.tsx
@@ -1,12 +1,21 @@
 import Link from "next/link"
+import searchPokemons from "../components/searchPokemons"
 
 export default function pokedex() {
+
+  const url = `https://pokeapi.co/api/v2/pokemon/pikachu`
+
+  searchPokemons(url)
+
   return (
     <div>
       <h1>First Post</h1>
       <h2>
         <Link href="/">Back to home</Link>
       </h2>
+
+      {/* {a && a.map((obj: any) => <div key={obj.id}>NOME: {obj.id}</div>)} */}
+      {/* {a ? (<div>NOME: {a.name}</div>)} */}
     </div>
   )
 }
---
+++ pages/styles/Nav.tsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+const Nav = styled.nav`
+  border: none;
+  background: #e03a3a;
+  color: #fff;
+  width: 100%;
+  height: 70px;
+  text-align: center;
+  display: flex;
+  justify-content: space-between;
+`;
+export default Nav;
